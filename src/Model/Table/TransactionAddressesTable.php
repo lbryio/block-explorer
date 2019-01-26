b96d40241d4f8b678e11a6f5f31e02ea25a9ab11
marcdeb1 <marcdebrouchev@laposte.net>
2019-01-26 21:04:46 +0100

    Name Transaction Address table file

---
+++ src/Model/Table/TransactionAddressesTable.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class TransactionAddressesTable extends Table {
+    public function initialize(array $config) {
+        parent::initialize($config);
+
+        $this->table('transaction_address');
+        
+        $this->addBehavior('SimpleAudit');
+    }
+}
+
+?>
