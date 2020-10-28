ecbd1c82cf1b3854789b4de0cfd6c722ab6dc86a
Akinwale Ariwodola <akinwale@gmail.com>
2020-10-28 15:55:11 +0100

    skip more claims query

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -226,8 +226,10 @@ class MainController extends AppController {
                 ])->select(['publisher' => 'C.name'])->leftJoin(['C' => 'claim'], ['C.claim_id = Claims.publisher_id'])->where(['Claims.claim_type' => 1, 'Claims.id <>' => $claim->id, 'Claims.publisher_id' => isset($claim->publisher) ? $claim->publisher_id : $claim->claim_id])->limit(9);
                 if (isset($claim->publisher) && $claim->publisher_id !== 'f2cf43b86b9d70175dc22dbb9ff7806241d90780') { // prevent ORDER BY for this particular claim
                     $moreClaimsQuery = $moreClaimsQuery->order(['Claims.fee' => 'DESC', 'RAND()' => 'DESC']);
+                    $moreClaims = $moreClaimsQuery->toArray();
+                } else {
+                    $moreClaims = [];
                 }
-                $moreClaims = $moreClaimsQuery->toArray();
                 for ($i = 0; $i < count($moreClaims); $i++) {
                     if ($canConvert && $moreClaims[$i]->fee > 0 && $moreClaims[$i]->fee_currency == 'USD') {
                         $moreClaims[$i]->price = $moreClaims[$i]->fee / $priceInfo->price;
